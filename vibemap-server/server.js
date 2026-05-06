c68bb764e7cf2fb37d42a629294bb4fa6b707d50
guta <shimjunseop001@gmail.com>
2026-05-06 10:08:43 +0900

    serve sitemap and robots with proper content types

--- vibemap-server/server.js
+++ vibemap-server/server.js
@@ -29,6 +29,8 @@ const contentTypes = {
   ".js": "text/javascript; charset=utf-8",
   ".css": "text/css; charset=utf-8",
   ".json": "application/json; charset=utf-8",
+  ".txt": "text/plain; charset=utf-8",
+  ".xml": "application/xml; charset=utf-8",
   ".png": "image/png",
   ".jpg": "image/jpeg",
   ".jpeg": "image/jpeg",
